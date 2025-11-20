9c8b259c93babf3ae43f25033b026ddef723f6a1
coolexotic25 <66102155+coolexotic25@users.noreply.github.com>
2025-11-20 02:40:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 function checkUpdate() {
     const appID = document.getElementById('appID').value.trim();
-    if (!appID) return alert("Masukkan AppID dulu!");
+    if (! appID) return alert ("Enter AppID first!");
   
     const steamDBURL = `https://steamdb.info/app/${appID}/history`;
     window.open(steamDBURL, '_blank');
   
     document.getElementById('buildInfo').innerText =
-      `🔎 Silakan cek SteamDB secara manual:\nhttps://steamdb.info/app/${appID}/history/`;
+      ` Please check SteamDB manually:\nhttps://steamdb.info/app/${appID}//herited/`;
   }
   
   function downloadFares() {
     const appID = document.getElementById('appID').value.trim();
-    if (!appID) return alert("Masukkan AppID dulu!");
+    if (! appID) return alert ("Enter AppID first!");
     const url = `https://fares.top/${appID}.zip`;
   
     const a = document.createElement('a');
@@ -19,5 +19,3 @@ function checkUpdate() {
     a.download = `${appID}.zip`;
     a.click();
   }
-
-  
